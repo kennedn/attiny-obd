7b4eff0c3b7eb3e9f45402b77e8705beafed3fa7
kennedn <kennedn@msn.com>
2023-07-16 16:20:00 +0100

    Get elm327 communication working via USI UART

--- src/elm327/main.c
+++ src/elm327/main.c
@@ -1,30 +1,41 @@
-#define F_CPU 8000000
-#include "usi_uart.h"
+#define F_CPU 8000000UL
+#include <stdint.h>
+#include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/sleep.h>
 #include <util/delay.h>
 #include <string.h>
 
-int main( void ) {
-    const char echo_str[] = "ATZ\r";
+#include "USI_UART.h"
 
-    unsigned char i;
+char uart_response_buffer[UART_RX_BUFFER_SIZE];
 
-    uuart_flush_buffers();
-    uuart_init_receiver();
+int main(void) {
 
-    // Enable global interrupts
-    cli();
-    _delay_ms(1000);
-    for ( i = 0; i < strlen( echo_str ); i++ ) {
-        uuart_tx_byte( (unsigned char)echo_str[i] );
-    }
-    // while(1) {
-    //     if ( uuart_data_in_rx_buffer() ) {
-    //         uuart_tx_byte( uuart_rx_byte() );
-    //     }
-    //     // sleep_enable();
-    // }
+    USI_UART_Initialise_Receiver();
+    sei();	// Enable global interrupts
+	// _delay_ms(1000);0
+	DDRB  |= _BV(PB3) | _BV(PB4);                         // Configure PB3 as output.
+    // PORTB ^= _BV(PB4);
+	
+	// for (;;) {		
+		// if (USI_UART_Data_In_Receive_Buffer()) {	// Check if there is data in the Rx buffer
+		    // unsigned char b = USI_UART_Receive_Byte();	// Receive a byte.
+        // _delay_ms(10000);	// Wait a while for all the data to come
+        // USI_UART_Receive_Enable(0);
+        USI_UART_Transmit_CString("ATZ\r"); // Reset 
+        while(USI_UART_Receive_Byte() != '>');
 
-    return 0;
+        USI_UART_Transmit_CString("ATE0\r"); // Echo off
+        while(USI_UART_Receive_Byte() != '>');
+
+        USI_UART_Transmit_CString("ATRV\r"); // Battery Voltage
+        while(USI_UART_Receive_Byte() != '>');
+        USI_UART_Copy_Receive_Buffer(uart_response_buffer, 6);
+        USI_UART_Deactivate();
+        
+        PORTB ^= _BV(PB4);
+        USI_UART_Transmit_CString(uart_response_buffer);
+        // } 
+        _delay_ms(1000);
+	return 0;
 }
